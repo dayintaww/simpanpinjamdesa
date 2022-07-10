1d3bd640d1abe937649ec0dc72e6fe3dc3dc9bea
dayintaww <37305576+dayintaww@users.noreply.github.com>
2022-07-10 18:48:30 +0700

    Update SimpanPinjamDesaApplicationTests.java

--- simpan-pinjam-desa/src/test/java/com/spd/simpanpinjamdesa/SimpanPinjamDesaApplicationTests.java
+++ simpan-pinjam-desa/src/test/java/com/spd/simpanpinjamdesa/SimpanPinjamDesaApplicationTests.java
@@ -1,25 +1,46 @@
 package com.spd.simpanpinjamdesa;
 
+import com.spd.simpanpinjamdesa.controller.SimpanPinjamController;
+import com.spd.simpanpinjamdesa.model.Anggota;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.runner.RunWith;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.hamcrest.core.Is.is;
 
-@ExtendWith(SpringExtension.class)
-@SpringBootTest
+import java.util.Collections;
+import java.util.List;
+
+@RunWith(SpringRunner.class)
 @AutoConfigureMockMvc
 class SimpanPinjamDesaApplicationTests {
 
     @Autowired
     private MockMvc mockMvc;
 
+    private SimpanPinjamController simpanPinjamController;
     @Test
-    void contextLoads() {
-    }
+    public void getAllAnggota() throws Exception {
+        Anggota anggota = new Anggota();
+        anggota.setIdAnggota("1");
+        anggota.setNamaAnggota("Nama Anggota");
 
+        List<Anggota> anggotaList = Collections.singletonList(anggota);
+
+        Mockito.when(simpanPinjamController.getAllAnggota()).thenReturn(anggotaList);
+
+        mockMvc.perform(MockMvcRequestBuilders
+                        .get("/getAllAnggota")
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$[0].name", is(anggota.getIdAnggota())));
+
+    }
 }
